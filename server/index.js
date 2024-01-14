53d312aac0e41a3b15d92007257032a9671a61b8
きよたか <c4mo.sw@gmail.com>
2024-01-14 18:55:24 -0500

    test(res): Testing server response

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("https://bulls-and-cows-server.vercel.app/", (req, res) => {
-  res.send("Hola")
-})
+app.get("https://bulls-and-cows-server.vercel.app", (req, res) => {
+  res.header(
+    "Acces-Control-Allow-Origin",
+    "https://bulls-and-cows-server.vercel.app"
+  );
+  res.send("Hola");
+});
 
 app.listen(PORT, () => console.log(`>> Server on PORT ${PORT} <<`));
