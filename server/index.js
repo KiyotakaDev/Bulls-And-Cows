d6979fa38e136b1e00b31ef5d45815b36184ec43
きよたか <c4mo.sw@gmail.com>
2024-01-14 18:53:48 -0500

    test(res): Testing server response

--- server/index.js
+++ server/index.js
@@ -3,4 +3,8 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT;
 
+app.get("https://bulls-and-cows-server.vercel.app/", (req, res) => {
+  res.send("Hola")
+})
+
 app.listen(PORT, () => console.log(`>> Server on PORT ${PORT} <<`));
