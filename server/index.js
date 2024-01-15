4502c5bbaa7d99b1de389f82b08752251b13a4e7
きよたか <c4mo.sw@gmail.com>
2024-01-14 19:27:17 -0500

    test(res): Testing server response

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT;
-const ALLOW_ORIGINS = process.env.ALLOW_ORIGINS
+const ALLOW_ORIGINS = process.env.ALLOW_ORIGINS;
 
-app.use(cors({
-  origin: ALLOW_ORIGINS,
-  credentials: true
-}))
+// app.use(cors({
+//   origin: ALLOW_ORIGINS,
+//   credentials: true
+// }))
 
 app.get("/", (req, res) => {
+  res.header(
+    "Allow-Control-Access-Origin",
+    "https://bulls-and-cows-client.vercel.app/"
+  );
   res.send("Hola");
 });
 
