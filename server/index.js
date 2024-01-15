93b4ccb8a5ec5dce31d383ba8b03c88f7f1438ba
きよたか <c4mo.sw@gmail.com>
2024-01-14 19:46:27 -0500

    test(cors): Testing server response with cors

--- server/index.js
+++ server/index.js
@@ -9,13 +9,12 @@ const PORT = process.env.PORT;
 const ALLOW_ORIGIN =
   ENV === "DEV" ? DEV_ORIGIN : ENV === "PROD" ? PROD_ORIGIN : null;
 
-// app.use(cors({
-//   origin: ALLOW_ORIGINS,
-//   credentials: true
-// }))
+app.use(cors({
+  origin: ALLOW_ORIGIN,
+  credentials: true
+}))
 
 app.get("/", (req, res) => {
-  res.header("Allow-Control-Access-Origin", ALLOW_ORIGIN);
   res.send("Server is running well :D");
 });
 
