2f020373cc51983521fe9f389b992c225865402b
きよたか <c4mo.sw@gmail.com>
2024-01-23 13:42:51 -0500

    feat(chat): Creating chat functionality

--- client/src/pages/GamePage.jsx
+++ client/src/pages/GamePage.jsx
@@ -2,21 +2,56 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 const GamePage = () => {
-  const [recievedMessage, setRecievedMessage] = useState("")
+  const [setRecievedMessage] = useState("");
+  const [messageToSend, setMessageToSend] = useState("");
+  const [allMessages, setAllMessages] = useState([]);
 
   const socket = io("http://localhost:3000/");
-  
+
   useEffect(() => {
-    socket.emit("message", (data) => {
-      const { message } = data
-      setRecievedMessage(message)
-    })
-  }, [])
-  
+    socket.on("message", (data) => {
+      const { message } = data;
+      setRecievedMessage(message);
+
+      setAllMessages((prevMessages) => [...prevMessages, message]);
+    });
+  }, []);
+
+  useEffect(() => {
+    socket.emit("message", { message: messageToSend });
+  }, [messageToSend]);
 
   return (
-    <div className="h-screen w-full bg-leveled-800 text-white flex flex-col justify-center items-center">
-      <p>Server message: {recievedMessage}</p>
+    <div className="h-screen w-full bg-leveled-800 text-white flex flex-col justify-center items-center gap-y-6">
+      <form
+        className="flex flex-col justify-center items-center gap-y-4"
+        onSubmit={(event) => {
+          event.preventDefault();
+          const msg = event.target[0].value;
+          setMessageToSend(msg);
+          setAllMessages((prevMessage) => [...prevMessage, msg]);
+          event.target[0].value = "";
+        }}
+      >
+        <input
+          className="bg-transparent outline-none border-2 border-white p-3 rounded-md"
+          placeholder="Message"
+        />
+        <button
+          type="submit"
+          className="bg-leveled-600 w-20 h-10 rounded-md hover:"
+          children="Send"
+        />
+      </form>
+      <div className="bg-leveled-900 h-1/2 overflow-y-scroll w-11/12 border-2 border-leveled-600">
+        <ul className="flex flex-col-reverse justify-center items-center gap-y-2 text-2xl">
+          {allMessages.map((msg, index) => (
+            <li className="w-11/12 text-center bg-leveled-800" key={index}>
+              {msg}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
