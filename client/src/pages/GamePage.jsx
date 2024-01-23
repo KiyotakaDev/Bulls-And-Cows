d5a7bfa6c0edd827600ae1c71252811ac2f602fd
きよたか <c4mo.sw@gmail.com>
2024-01-23 12:11:43 -0500

    feat(socket): Implementing socket connection

--- client/src/pages/GamePage.jsx
+++ client/src/pages/GamePage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 const GamePage = () => {
-  return (
-    <div>GamePage</div>
-  )
-}
+  const [recievedMessage, setRecievedMessage] = useState("")
 
-export default GamePage
+  const socket = io("http://localhost:3000/");
+  
+  useEffect(() => {
+    socket.emit("message", (data) => {
+      setRecievedMessage(data)
+    })
+  }, [])
+  
+
+  return (
+    <div className="h-screen w-full bg-leveled-800 text-white flex flex-col justify-center items-center">
+      <p>Server message: {recievedMessage}</p>
+    </div>
+  );
+};
+
+export default GamePage;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        leveled: {
+          50: "#e3b5b5",
+          100: "#dcacba",
+          200: "#d28eae",
+          300: "#c261a1",
+          400: "#9d3f8d",
+          500: "#6f2f6f",
+          600: "#4b1e52",
+          700: "#2d1537",
+          800: "#1a0f24",
+          900: "#110b18",
+          950: "#040308",
+        }
+      }
+    },
   },
   plugins: [],
 }
