a045116cd13f71b4fe1a9b5e158a6fa0cf70058e
Draakox <draakox.dev@gmail.com>
2024-01-23 15:24:33 -0500

    create rooms

--- server/controllers/handleClientMessage.js
+++ server/controllers/handleClientMessage.js
@@ -1,10 +1,26 @@
+const salas = new Set();
+
 const controller = (socket, io) => {
-  const sliced = socket.id.substring(0, 3)
+  const sliced = socket.id.substring(0, 3);
 
   console.log("New client connected " + sliced);
 
+  socket.on("createNewRoom", (roomName) => {
+    if (salas.has(roomName)) {
+      console.log("Nombre de sala ya existente");
+    } else {
+      salas.add(roomName);
+      socket.join(roomName);
+
+      const totalCliens = io.sockets.adapter.rooms.get(roomName).size;
+      console.log("Personas en la sala" + totalCliens);
+    }
+  });
+
   socket.on("message", (message) => {
-    io.emit("message", message);
+    const rooms = Object.keys(socket.rooms);
+    const currentRoom = rooms[1];
+    io.to(currentRoom).emit("message", message);
   });
 
   socket.on("disconnect", () => {
